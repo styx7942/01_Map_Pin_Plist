48bd4ff6328d67d824089ac6640a62c7a0801b62
D7703_11 <d7703_11@d7703-11ui-imac.local>
2017-09-19 11:52:41 +0900

    ll

--- 01_Map_Pin_Plist/ViewController.swift
+++ 01_Map_Pin_Plist/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myMapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        zoomToRegion()
+        
+        //plist 화일을 프로그램에 가져오기
+        let path = Bundle.main.path(forResource: "ViewPoint3", ofType: "plist")
+        print("path = \(String(describing: path))")
+        
+        //이 프로그램에서 ViewPoint3.Plist를 사용하기 위해 복사하기
+        let contents = NSArray(contentsOfFile: path!)
+        print("contents = \(String(describing: contents))")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    func zoomToRegion(){
+        let center = CLLocationCoordinate2DMake(35.166197, 129.072594)//중심
+        let span = MKCoordinateSpanMake(0.05, 0.05)//반경
+        let region = MKCoordinateRegion(center: center, span: span)
+        myMapView.setRegion(region, animated: true)
     }
-
-
 }
 
